7e5dc36380d86db6c0108cccfa95640eaa616208
Allan Vila <R.AllanVila@gmail.com>
2021-04-11 02:45:41 -0400

    first commit

---
+++ assets/js/components/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Header extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: 'Joe'
+		};
+	}
+	clickedBtn = () => {};
+	async test() {}
+	render() {
+		return (
+			<header>
+				<div>Logo</div>
+				<nav>
+					<a href="#">Create Ads</a>
+					<a href="#">About Us</a>
+					<a href="#">Login</a>
+					<a href="#" className="register-btn">
+						Register
+					</a>
+				</nav>
+			</header>
+		);
+	}
+}
+
+const app = document.getElementById('app');
